2af9fe3fda06b36a7e8b6fd4928b15fe02aaee89
Jeff Wu <crazy.alu@outlook.com>
2017-01-09 23:24:43 +0800

    resolve the issue about removeAt method from formArray instance and using formGroup directive to directly bind formArray.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +7,22 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  form;
+  form: FormGroup;
   emailPattern = '^[a-zA-Z0-9.!#$%&』*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$';
 
   constructor(private _fb: FormBuilder) {
-    this.form = this._fb.array([
-      this.buildGroup()
-    ]);
+    this.form = this._fb.group({
+      formAr: this._fb.array([
+        this.buildGroup()
+      ])
+    });
   }
 
   buildGroup(): FormGroup {
     return this._fb.group({
-      firstName: ['', [Validators.required, Validators.minLength(5)]],
+      firstName: '',
       nickName: 'crazy',
-      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+      email: '',
       phone: '0912345678',
       birthday: '',
       interest: this._fb.group({
@@ -35,7 +37,13 @@ export class AppComponent {
   }
 
   add() {
-    this.form.push(this.buildGroup());
+    const control = <FormArray>this.form.controls['formAr'];
+    control.push(this.buildGroup());
   }
 
+  remove(i: number) {
+    // remove address from the list
+    const control = <FormArray>this.form.controls['formAr'];
+    control.removeAt(i);
+  }
 }
